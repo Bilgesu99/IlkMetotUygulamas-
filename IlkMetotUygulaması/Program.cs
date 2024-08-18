f8b8455a2e54637d25437c31a1abbab55ca7335d
asus <asus@DESKTOP-OTTG4TB>
2024-08-18 12:42:04 +0300

    Add project files.

---
+++ IlkMetotUygulaması/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+
+        EnSevdiğinŞarkıSözü();
+        int kalan = RastgeleSayı();
+        Console.WriteLine("Rastgele sayının 2'ye bölümünden kalan: " + kalan);
+        int çarpım = SayılarınÇarpımı(4, 5);
+        Console.WriteLine("Sayıların çarpımı: " + çarpım);
+        HoşgeldinMesajı("Bilge Su", "Kuzucular");
+    }
+
+    static void EnSevdiğinŞarkıSözü()
+    {
+        Console.WriteLine("Lady in red");
+    }
+
+    static int RastgeleSayı()
+    {
+        Random rnd= new Random();
+        int number = rnd.Next(1, 100);
+        return number % 2;
+    }
+    static int SayılarınÇarpımı(int x, int y)
+    {
+        return x * y;
+    }
+
+    static void HoşgeldinMesajı(string adı, string soyadı)
+    {
+        Console.WriteLine("Hoşgeldiniz " + adı + " " + soyadı + " ");
+    }
+
+
+
+}
+
